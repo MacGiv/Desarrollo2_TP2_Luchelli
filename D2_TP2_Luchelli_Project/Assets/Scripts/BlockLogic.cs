4ab10d17ee69cf07d132a12a4ac9726c954aedf1
MacGiv <tomasluchelli.17@gmail.com>
2026-05-06 19:36:36 -0300

    Add Block Placement Logic

---
+++ D2_TP2_Luchelli_Project/Assets/Scripts/BlockLogic.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class BlockLogic : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    
+    private bool hasLanded = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!hasLanded)
+        {
+            if (collision.gameObject.CompareTag("Base"))
+            {
+                hasLanded = true;
+
+                EvaluatePlacement(collision);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Evaluates how well the block was placed
+    /// </summary>
+    private void EvaluatePlacement(Collision collision)
+    {
+        Transform other = collision.transform;
+
+        float offset = Mathf.Abs(transform.position.x - other.position.x);
+
+        Debug.Log($"Offset: {offset}");
+
+        if (offset < 0.2f)
+        {
+            Debug.Log("Perfect placement!");
+
+        }
+        else if (offset < 0.5f)
+        {
+            Debug.Log("Good placement");
+        }
+        else
+        {
+            Debug.Log("Miss!");
+            Die();
+            return;
+        }
+
+        PlaceBlock(collision);
+    }
+
+    /// <summary>
+    /// Placement logic
+    /// </summary>
+    private void PlaceBlock(Collision collision)
+    {
+        rb = GetComponent<Rigidbody>();
+        //Set Block tag to Base's tag for next collisions
+        gameObject.tag = collision.gameObject.tag;
+        // Freeze Block's rotation and movement and set all constriants to true
+        rb.freezeRotation = true;
+        rb.angularVelocity = Vector3.zero;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    /// <summary>
+    /// Destroy logic when misplaced
+    /// </summary>
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
