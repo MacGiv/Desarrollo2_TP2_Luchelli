651f0f84818b231af73b25d8ea3ccca9fcb41691
MacGiv <tomasluchelli.17@gmail.com>
2026-05-08 18:43:07 -0300

    Update Data_Block Script

--- D2_TP2_Luchelli_Project/Assets/Scripts/Data_Block.cs
+++ D2_TP2_Luchelli_Project/Assets/Scripts/Data_Block.cs
@@ -6,14 +6,11 @@ public class Data_Block : ScriptableObject
 {
     [Header("Physics")]
     public float mass = 1f;
-
     public float drag = 0f;
-
     public float angularDrag = 0.05f;
 
     [Header("Placement")]
     public float perfectOffsetThreshold = 0.2f;
-
     public float goodOffsetThreshold = 0.5f;
 
     [Header("Visual")]
